221978e5bd5119857848a07a24ebdce4e750ecf3
Victoria <vigodfrey@mail.ru>
2024-02-01 00:11:56 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+ {
+     public static int RecursiveAckerman(int m, int n)
+     {
+         while (m != 0) 
+         {
+             if (n == 0)
+             {
+                 n = 1;
+             }
+             else
+             {
+                 n = RecursiveAckerman(m, n - 1);
+             }
+             m--;
+         }
+         return n + 1;
+     }
+     public static void Main(string[] args)
+     {
+         Console.Write(RecursiveAckerman(3, 5));
+         Console.ReadKey(); // Для того чтобы консоль не закрывалась раньше времени
+     }
+ }
