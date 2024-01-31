c764866acc6f2ef0b9c54edc4f99ae5fa2d8bc5f
Victoria <vigodfrey@mail.ru>
2024-02-01 00:14:11 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void RecursiveArrayDisplay(int[] arr, int i)
+        {
+            if (i < 0) return;
+
+            Console.WriteLine(arr[i]);           
+
+            RecursiveArrayDisplay(arr, i - 1);
+           
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1,2,3,4,5,6 };
+
+            RecursiveArrayDisplay(arr, arr.Length - 1);
+            Console.ReadKey(); // Для того чтобы консоль не закрывалась раньше времени
+        }
+    }
+}
