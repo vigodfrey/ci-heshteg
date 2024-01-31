e454ecb52acf03163609faa1f9c2d04f3b932e79
Victoria <vigodfrey@mail.ru>
2024-02-01 00:05:55 +0300

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void RecursiveSequence(int m, int n)
+        {
+            if (m > n) return;
+
+            Console.WriteLine(m);
+
+            RecursiveSequence(m + 1, n);
+
+        }
+        public static void Main(string[] args)
+        {
+            RecursiveSequence(1, 10);
+            Console.ReadKey(); // Для того чтобы консоль не закрывалась раньше времени
+        }
+    }
+}
